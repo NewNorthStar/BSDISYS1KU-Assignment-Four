04e0dcff00b0945852fee1d83f7cc9e4fc6d450d
Ronas Olsen <rono@itu.dk>
2024-11-05 12:03:54 +0100

    Enter, exit and reply methods.

--- Ricard/ricard.go
+++ Ricard/ricard.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	proto "example.com/ricard/grpc"
@@ -10,6 +11,7 @@ import (
 var myProcessNumber int64 = time.Now().Unix()
 var myTime int64 = 1
 var myState state = RELEASED
+var queue chan bool = make(chan bool)
 
 type state int64
 
@@ -28,5 +30,45 @@ func main() {
 }
 
 func (s *RicardServer) Request(ctx context.Context, msg *proto.Message) (*proto.Empty, error) {
+	if myState == HELD || myState == WANTED && comesAfterMe(msg) {
+		<-queue
+	}
+	return &proto.Empty{}, nil
+}
 
+func enter() {
+	myState = WANTED
+	var replies sync.WaitGroup
+
+	for i := 0; i < 3; i++ {
+		replies.Add(1)
+
+		go func() {
+			defer replies.Done()
+			// Contact other node
+		}()
+	}
+
+	replies.Wait()
+	myState = HELD
+}
+
+func exit() {
+	myState = RELEASED
+main:
+	for {
+		select {
+		case queue <- true:
+		default:
+			break main
+		}
+	}
+}
+
+func comesAfterMe(msg *proto.Message) bool {
+	if myTime != msg.Time {
+		return myTime < msg.Time
+	} else {
+		return myProcessNumber < msg.Process
+	}
 }
